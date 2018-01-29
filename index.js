d9303a838ef2ff35630ca4e9c677c27d3f0d3382
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-29 17:27:24 +0530

    Frontend added

---
+++ index.js
@@ -0,0 +1,25 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
+
+server.register(require('inert'), (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler(request, reply) {
+      reply.file('./public/login.html');
+    },
+  });
+});
